769ae67d0e63d1a833005f1fa367462c2ad8d092
mairon.vilela <maironvilela@hotmail.com>
2024-07-30 13:40:25 -0300

    refactor: image about file

--- app/components/banner/index.tsx
+++ app/components/banner/index.tsx
@@ -5,15 +5,15 @@ export const Banner = () => {
   return (
     <section className="container bg-banner-image  bg-cover">
       <Header />
-      <div className="container flex flex-col gap-5 py-10 items-center">
-        <h1 className="text-center text-2xl">
+      <div className="container flex flex-col gap-5 py-10 items-center ">
+        <h1 className="text-center text-2xl max-w-[700px]">
           SABERES PERPÉTUOS DO REINADO DE ITAPECERICA:{' '}
           <span className="text-emerald-400">
             UM OLHAR SOBRE AS APRENDIZAGENS DA TRADIÇÃO
           </span>
         </h1>
 
-        <p className="font-mono text-center text-sm">
+        <p className="font-mono text-center text-sm max-w-[700px]">
           Dissertação de mestrado apresentada ao Programa de Pós-Graduação em
           Educação e Formação Humana da Universidade do Estado de Minas Gerais,
           como requisito parcial para obtenção do título de mestre em Educação
---
+++ app/components/latest-videos/index.tsx
@@ -0,0 +1,19 @@
+export const LatestVideos = () => {
+  const LATEST_VIDEOS = [
+    {
+      cover: 'cover-1.jpg',
+      linkUrl: '',
+      description: '',
+    },
+    {
+      cover: 'cover-2.jpg',
+      linkUrl: '',
+      description: '',
+    },
+    {
+      cover: 'cover-3.jpg',
+      linkUrl: '',
+      description: '',
+    },
+  ]
+}
