4ea614d15554ae1050f551577db8b54745e96764
mairon.vilela <maironvilela@hotmail.com>
2024-07-30 13:40:25 -0300

    refactor: image about file

--- app/components/about/index.tsx
+++ app/components/about/index.tsx
@@ -2,27 +2,33 @@ import Image from 'next/image'
 
 export const About = () => {
   return (
-    <section className="container flex flex-row">
-      <div className="flex flex-col gap-3">
-        <h2 className="font-mono py-10 ">Sobre o projeto</h2>
-        <p className="max-w-[200]">
-          Esse canal foi criado para compartilhar imagens coletadas durante
-          minha pesquisa de mestrado intitulada{' '}
-          <strong className="text-emerald-400">
-            Saberes Perpétuos do Reinado de Itapecerica
-          </strong>
-          . As incursões de campo foram realizadas durante os festejos de
-          reinado realizados na cidade de Itapecerica, localizada no centro
-          oeste de Minas Gerais.
-        </p>
+    <section className="container flex flex-col gap-4">
+      <div className="flex flex-row gap-2 justify-center ">
+        <div className="flex flex-col gap-3  ">
+          <h2 className="font-mono py-5 ">Sobre o projeto</h2>
+          <p className="w-[380px]">
+            Esse canal foi criado para compartilhar imagens coletadas durante
+            minha pesquisa de mestrado intitulada{' '}
+            <strong className="text-emerald-400">
+              Saberes Perpétuos do Reinado de Itapecerica
+            </strong>
+            .
+          </p>
+        </div>
+        <Image
+          className="pt-5"
+          src="/images/matriz-de-sao-bento.jpg"
+          width={200}
+          height={420}
+          alt={'foto da Matriz de São Bento de Itapecerica'}
+        />
       </div>
-      <Image
-        className="py-2"
-        src="/images/matriz-de-sao-bento.jpg"
-        width={200}
-        height={200}
-        alt={'foto da Matriz de São Bento de Itapecerica'}
-      />
+
+      <p>
+        As incursões de campo foram realizadas durante os festejos de reinado
+        realizados na cidade de Itapecerica, localizada no centro oeste de Minas
+        Gerais.
+      </p>
     </section>
   )
 }
